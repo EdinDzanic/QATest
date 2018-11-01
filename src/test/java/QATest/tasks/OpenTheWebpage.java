4e499753cfb733eeb35d35c2f1e5c43f35355453
Enis <enis.jahic@gmail.com>
2018-11-01 09:58:49 +0100

    add .gitignore

--- src/test/java/QATest/tasks/OpenTheWebpage.java
+++ src/test/java/QATest/tasks/OpenTheWebpage.java
@@ -4,16 +4,16 @@ import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Open;
 import net.thucydides.core.annotations.Step;
-import QATest.ui.GoogleSearchPage;
+import QATest.ui.RubiconQAPage;
 
-public class OpenTheApplication implements Task {
+public class OpenTheWebpage implements Task {
 
-    GoogleSearchPage googleSearchPage;
+    RubiconQAPage rubiconQAPage;
 
     @Step("Open the application")
     public <T extends Actor> void performAs(T actor) {
         actor.attemptsTo(
-                Open.browserOn().the(googleSearchPage)
+                Open.browserOn().the(rubiconQAPage)
         );
     }
 }
--- src/test/java/QATest/ui/RubiconQAPage.java
+++ src/test/java/QATest/ui/RubiconQAPage.java
@@ -3,5 +3,5 @@ package QATest.ui;
 import net.serenitybdd.core.pages.PageObject;
 import net.thucydides.core.annotations.DefaultUrl;
 
-@DefaultUrl("http://www.google.co.uk")
-public class GoogleSearchPage extends PageObject {}
+@DefaultUrl("https://enisjahic.github.io/rubiconQA/index.html")
+public class RubiconQAPage extends PageObject {}
